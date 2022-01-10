131cb7d017662a0663e1b7d83247868730fadd61
Salman Sourav <sunnysourav07@gmail.com>
2022-01-10 10:46:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <img class="bg" src="bg.jpg" alt="IIT Kharagpur">
+    <img class="bg" src="bg.jpg" alt="Cox's Bazar">
     <div class="container">
         <h1>Welcome to the Annual Cox's Bazar Tour</h3>
         <p>Enter your details and submit this form to confirm your participation in the Tour </p>
